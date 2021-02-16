0296d051cc2c577a00db80c57f7dbbc259aec0b5
Paula Lenis <paulalenisb@gmail.com>
2021-02-15 22:54:14 -0500

    feat: grid

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const photoArr = (data) => {
 
 const loadImg = () => {
 
-    fetch(`${Api.base}?query='brand'&perpage=20&client_id=${Api.key}`)
+    fetch(`${Api.base}?query='brand'&perpage=10&client_id=${Api.key}`)
     .then(res => {
         if(res.ok) return res.json()
         else console.log(res.status);
